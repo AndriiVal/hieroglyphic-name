205d2c2047f60424c597d62ead482ad783406d6b
Andrii Valchuk <andriy.valchuk@gmail.com>
2020-11-10 18:01:12 +0200

    change size images js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function showHieroglyph(){
 	for (let i = 0; i < str.length; i++) {
 		if (filter.indexOf(str[i]) !== -1) {
 			if (window.matchMedia("(orientation: portrait)").matches) {
-				$('#hieroglyph').append(`<p><img src="img/${str[i]}.png" style="width:200px"></p>`);
+				$('#hieroglyph').append(`<p><img src="img/${str[i]}.png" style="width:150px"></p>`);
 			} else{
 				$('#hieroglyph').append(`<img src="img/${str[i]}.png" style="width:100px">`);
 			}
